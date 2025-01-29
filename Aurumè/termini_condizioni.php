d3e5e07cd3112782e03ea1544b318dc71d7c46cc
Carmine Terracciano <carminet2004@gmail.com>
2025-01-29 16:53:37 +0100

    correzione nomi file

--- Aurumè/termini_condizioni.php
+++ Aurumè/termini_condizioni.php
@@ -26,6 +26,7 @@
             <a href="#"><i class="fas fa-shopping-bag"></i></a>
         </div>
     </header>
+
     <main>
         <section>
             <h1>Termini & condizioni generali</h1>
@@ -33,20 +34,20 @@
                 agli acquisti di prodotti e alla condivisione di informazioni tra Aurumè e il cliente che intende ordinare e 
                 acquistare prodotti tramite la sezione italiana del sito internet di Aurumè.
             </p>
-            <p>L’utilizzo del Sito per l’ordine o l’acquisto di prodotti implica la Sua accettazione delle presenti Condizioni generali, 
+            <p>L'utilizzo del Sito per l'ordine o l'acquisto di prodotti implica la Sua accettazione delle presenti Condizioni generali, 
                 e della Informativa sulla privacy richiamati integralmente, nonché di altri avvisi che potrebbero essere pubblicati 
                 sul Sito nel momento in cui viene inoltrato un ordine e infine di tutte le leggi e i regolamenti applicabili. 
                 Le presenti Condizioni generali forniscono informazioni su chi siamo, come è possibile acquistare prodotti sul Sito, 
                 come cancellare un ordine, cosa fare in caso di problemi ed altre informazioni importanti. 
                 La preghiamo di consultare attentamente le seguenti Condizioni generali prima di effettuare un ordine tramite il Sito. 
-                Qualora non sia d’accordo con le condizioni descritte nelle presenti Condizioni generali, 
-                nei Termini d’uso o nell’Informativa sulla privacy la invitiamo a non effettuare ordini tramite il Sito.
+                Qualora non sia d'accordo con le condizioni descritte nelle presenti Condizioni generali, 
+                nei Termini d'uso o nell'Informativa sulla privacy la invitiamo a non effettuare ordini tramite il Sito.
             </p>
         </section><br>
         <section>
             <h2>Ordini</h2>
             <p>Per effettuare ordini sul Sito, deve essere maggiorenne e dotato di capacità giuridica. 
-                Lei potrà effettuare l’ordine tramite il Sito e effettuare il pagamento del prezzo del prodotto/i scelto/i, 
+                Lei potrà effettuare l'ordine tramite il Sito e effettuare il pagamento del prezzo del prodotto/i scelto/i, 
                 seguendo le indicazioni presenti sul Sito stesso.
                 È possibile effettuare un ordine tramite il Sito e scegliere il servizio di spedizione.
             </p>
@@ -54,13 +55,13 @@
         <section>
             <h2>Conferma degli ordini</h2>
             <p>Per effettuare un ordine, Lei dovrà selezionare i prodotti che desidera acquistare e cliccare su “Aggiungi al carrello”. 
-                I prodotti selezionati saranno quindi aggiunti al “carrello” e l’ordine potrà essere concluso compilando 
-                l’apposito modulo ed accettando le Condizioni generali di vendita come indicato sul Sito. 
-                Prima di concludere l’ordine, dovrà verificare l’accuratezza delle informazioni fornite e 
-                correggere eventuali inesattezze. L’ordine verrà inviato cliccando sul tasto “inoltra l’ordine”.
+                I prodotti selezionati saranno quindi aggiunti al “carrello” e l'ordine potrà essere concluso compilando 
+                l'apposito modulo ed accettando le Condizioni generali di vendita come indicato sul Sito. 
+                Prima di concludere l'ordine, dovrà verificare l'accuratezza delle informazioni fornite e 
+                correggere eventuali inesattezze. L'ordine verrà inviato cliccando sul tasto “inoltra l'ordine”.
             </p>
-            <p>Una volta inviato, l’ordine verrà preso in carico da Aurumè.
-                Il contratto tra Lei e Aurumè è concluso solo al momento dell’invio della Conferma dell’ordine e 
+            <p>Una volta inviato, l'ordine verrà preso in carico da Aurumè.
+                Il contratto tra Lei e Aurumè è concluso solo al momento dell'invio della Conferma dell'ordine e 
                 riguarderà solo i prodotti il cui invio è stato confermato. Aurumè conserva le transazioni con i propri clienti. 
                 Lei è invitato a scaricare le presenti Condizioni generali. In caso di controversia 
                 relativa ad un ordine effettuato sul Sito, i dati conservati da Aurumè costituiranno prova della transazione.
@@ -80,17 +81,17 @@
                         <li>non abbia effettuato il pagamento dei prodotti acquistati alla scadenza del termine previsto,</li>
                         <li>sia coinvolto in attività fraudolente o di mercato grigio (i.e. rivendita di prodotti mediante 
                             canali non appartenenti alla rete di vendita autorizzata di Aurumè o delle sue affiliate),</li>
-                        <li>si rifiuti di fornire delle informazioni che Aurumè ha l’obbligo di raccogliere per legge;</li>
+                        <li>si rifiuti di fornire delle informazioni che Aurumè ha l'obbligo di raccogliere per legge;</li>
                     </ul>
                 </li>
-                <li>Aurumè ritenesse che la cancellazione dell’ordine sia obbligatoria in base alla legge;</li>
+                <li>Aurumè ritenesse che la cancellazione dell'ordine sia obbligatoria in base alla legge;</li>
                 <li>Aurumè abbia il diritto di cancellare un ordine ai sensi delle presenti Condizioni generali.</li>
             </ul>
             <p>Nel caso in cui Aurumè annulli un ordine, rimborserà ogni pagamento precedentemente effettuato.
                 Aurumè fa il possibile affinché il colore, il design e lo stile dei prodotti Aurumè raffigurati sul Sito 
                 siano rappresentativi al meglio dei prodotti originali.
             </p>
-            <p>Nonostante ciò, l’aspetto del prodotto originale potrebbe differire leggermente rispetto a quanto 
+            <p>Nonostante ciò, l'aspetto del prodotto originale potrebbe differire leggermente rispetto a quanto 
                 raffigurato sul Sito per motivi tecnici o per altre motivazioni.
                 Aurumè, nei limiti di quanto consentito dalla legge, non sarà pertanto responsabile per eventuali errori o 
                 inesattezze nelle fotografie o nelle rappresentazioni grafiche dei prodotti Aurumè visualizzati sul Sito, 
@@ -102,33 +103,33 @@
                 MAIL.
                 Lei potrà acquistare fino ad un massimo di 10 prodotti per un valore massimo 
                 del singolo prodotto non superiore a 10.000 Euro.</p>
-        </section><br>>
+        </section><br>
         <section>
             <h2>Prezzi & pagamenti</h2>
             <p>I prezzi dei prodotti Aurumè sono indicati in Euro ed includono tutte le tasse applicabili. Sono escluse le spese di spedizione.</p>
             <p>Aurumè si riserva il diritto di modificare il prezzo dei prodotti visualizzati sul Sito in qualsiasi momento e senza alcuna previa comunicazione. 
-                Le sarà addebitato il prezzo visualizzato sul Sito nel momento in cui l’ordine è stato inviato da Lei cliccando sul tasto “inoltra l’ordine”.
+                Le sarà addebitato il prezzo visualizzato sul Sito nel momento in cui l'ordine è stato inviato da Lei cliccando sul tasto “inoltra l'ordine”.
             </p>
-            <p>Per ciascun ordine effettuato tramite il Sito, Lei dovrà effettuare il pagamento al momento dell’effettuazione dell’ordine. 
-                L’importo dei prodotti ordinati sarà bloccato sulla Sua carta di credito ma sarà addebitato solo al momento dell’invio della Conferma dell’ordine da parte di Aurumè.
+            <p>Per ciascun ordine effettuato tramite il Sito, Lei dovrà effettuare il pagamento al momento dell'effettuazione dell'ordine. 
+                L'importo dei prodotti ordinati sarà bloccato sulla Sua carta di credito ma sarà addebitato solo al momento dell'invio della Conferma dell'ordine da parte di Aurumè.
             </p>
             <p>Sono accettati esclusivamente pagamenti tramite Stripe. Tutti gli ordini devono essere pagati in Euro. 
-                L’ordine deve contenere un indirizzo di consegna italiano. Non sarà possibile effettuare ordini con indirizzo di consegna non italiano.
+                L'ordine deve contenere un indirizzo di consegna italiano. Non sarà possibile effettuare ordini con indirizzo di consegna non italiano.
             </p>
             <p>Se si desidera ricevere una fattura per il proprio ordine, può contattare Aurumè tramite il servizio clienti, 
-                inviando una mail a MAIL, prima di concludere l’ordine.
+                inviando una mail a MAIL, prima di concludere l'ordine.
             </p>
         </section><br>
         <section>
             <h2>Consegna</h2>
-            <p>I prodotti acquistati tramite il Sito possono essere consegnati solo all’interno del territorio italiano. 
-                I prodotti saranno spediti all’indirizzo da Lei inserito durante il processo di ordinazione e indicato 
-                nella conferma dell’ordine. È possibile inserire un solo indirizzo per ogni ordine.
+            <p>I prodotti acquistati tramite il Sito possono essere consegnati solo all'interno del territorio italiano. 
+                I prodotti saranno spediti all'indirizzo da Lei inserito durante il processo di ordinazione e indicato 
+                nella conferma dell'ordine. È possibile inserire un solo indirizzo per ogni ordine.
                 Le caselle postali non possono essere utilizzate per la consegna dei nostri prodotti. 
-                Se si inserisce l’indirizzo di un Hotel come indirizzo di consegna dell’ordine, la consegna potrà essere 
-                effettuata esclusivamente alla persona indicata nell’ordine e a nessun altro, inclusa la reception dell’Hotel. 
+                Se si inserisce l'indirizzo di un Hotel come indirizzo di consegna dell'ordine, la consegna potrà essere 
+                effettuata esclusivamente alla persona indicata nell'ordine e a nessun altro, inclusa la reception dell'Hotel. 
                 È richiesta la Sua firma al momento della consegna.
-                I prodotti saranno spediti in un singolo pacco che include il prodotto, l’imballaggio, la ricevuta di pagamento 
+                I prodotti saranno spediti in un singolo pacco che include il prodotto, l'imballaggio, la ricevuta di pagamento 
                 o fattura ove richiesta, e, a seconda dei casi, la certificazione dei gioielli e certificato di garanzia.
                 Per ricevere il pacco è richiesta la firma di un maggiorenne.
             </p>
@@ -136,20 +137,20 @@
         <section>
             <h2>Tempi & costi spedizione</h2>
             <p>Salvo che per i prodotti acquisitati in pre-ordine e per i prodotti personalizzati, 
-                gli ordini verranno consegnati tramite consegna standard entro 1-2 giorni lavorativi dalla data di conferma dell’ordine, 
+                gli ordini verranno consegnati tramite consegna standard entro 1-2 giorni lavorativi dalla data di conferma dell'ordine, 
                 tranne in casi eccezionali che esulano dalla sfera di controllo di Aurumè e per i quali Lei verrà immediatamente contattato.</p>
             <p>Per i prodotti in pre-ordine, dovrà fare riferimento alla data di spedizione stimata come 
                 indicata nella pagina di descrizione del prodotto. Per i prodotti personalizzati la consegna avverrà 
-                entro 4 giorni lavorativi dalla data di conferma dell’ordine. 
+                entro 4 giorni lavorativi dalla data di conferma dell'ordine. 
                 La consegna dei prodotti acquistati tramite il Sito è gratuita.</p>
         </section><br>
         <section>
             <h2>Politica di reso</h2>
-            <p>a. Regole generali</p>
+            <h3>a. Regole generali</h3>
             <p>Può effettuare un reso di un prodotto acquistato tramite il Sito, e ottenere il rimborso del prezzo pagato o 
                 chiedere la sostituzione con un altro prodotto (di taglia o colore diversi), informandoci chiaramente della 
                 Sua intenzione di farlo entro quattordici (14) giorni di calendario dalla data di ricezione del prodotto: contattando il 
-                nostro servizio clienti all’indirizzo mail it.cs@aurume.com o al numero di telefono +39 0694801941 o tramite il Sito.</p>
+                nostro servizio clienti all'indirizzo mail it.cs@aurume.com o al numero di telefono +39 0694801941 o tramite il Sito.</p>
             <p>Tutti i resi devono contenere la ricevuta di vendita originale ed essere restituiti nella confezione originale. 
                 Il diritto di recesso è escluso per i prodotti personalizzati (quali ad esempio prodotti con incisioni) e le creazioni 
                 olfattive, che non potranno quindi essere oggetto di restituzione, rimborso né sostituzione.</p>
@@ -164,27 +165,27 @@
                 altro metodo che fornisca prova della data di consegna. Lei dovrà inoltre assicurare la restituzione del prodotto contro furti 
                 e perdita per il valore del prodotto.</p>
             <p>La nostra Politica di Reso non intende in alcun modo escludere o limitare diritti o rimedi inderogabili e a Lei spettanti in base alla legge.</p>
-            <p>b. Restituzione mediante il servizio clienti</p>
-            <p>Una volta ricevuta la comunicazione relativa alla Sua volontà di recedere dal contratto, Aurumè Le invierà un’etichetta 
+            <h3>b. Restituzione mediante il servizio clienti</h3>
+            <p>Una volta ricevuta la comunicazione relativa alla Sua volontà di recedere dal contratto, Aurumè Le invierà un'etichetta 
                 pre-pagata per la restituzione. Può anche restituire i prodotti inviandoli direttamente a Aurumè unitamente al Modulo di 
                 restituzione od altra dichiarazione della Sua volontà di recedere dal contratto. 
                 Dovrà restituire il prodotto entro 14 giorni dal momento della Sua comunicazione a Aurumè della volontà di recedere. 
                 Il suddetto termine è rispettato se la spedizione del prodotto è effettuata prima della scadenza dei 14 giorni. 
-                Il rimborso del prodotto restituito sarà effettuato sullo stesso mezzo di pagamento utilizzato per l’acquisto sul Sito. 
+                Il rimborso del prodotto restituito sarà effettuato sullo stesso mezzo di pagamento utilizzato per l'acquisto sul Sito. 
                 Non sarà possibile sostituire un prodotto con altro di valore inferiore e non sarà possibile chiedere in boutique 
                 né il rimborso né la sostituzione di un prodotto acquistato online. In caso di regali, il destinatario del regalo 
                 potrà esclusivamente chiedere la sostituzione del prodotto regalato con un altro di valore uguale o superiore. 
                 Le spese di spedizione, se pagate, saranno rimborsate. Nel caso di sostituzione di un prodotto acquistato sul Sito 
-                con un altro prodotto, non saranno addebitate le spese di spedizione per l’invio del prodotto in sostituzione.</p>
-            <p>c. Restituzione online</p>
-            <p>E’ anche possibile restituire i prodotti acquistati online, per ottenere un rimborso o una sostituzione, tramite il Sito. 
+                con un altro prodotto, non saranno addebitate le spese di spedizione per l'invio del prodotto in sostituzione.</p>
+            <h3>c. Restituzione online</h3>
+            <p>E' anche possibile restituire i prodotti acquistati online, per ottenere un rimborso o una sostituzione, tramite il Sito. 
                 Per effettuare un reso tramite il Sito, bisogna compilare il modulo di reso online o accedere alla pagina 
                 Cronologia ordini nella sezione MyAccount e seguire le relative indicazioni. 
                 A seguito della richiesta di reso, Aurumè Le invierà un'etichetta di reso prepagata. 
                 Se sceglie di non utilizzare l'etichetta di reso prepagata fornita da Aurumè, i rischi e le spese di assicurazione e 
                 spedizione saranno a Suo carico. Il rimborso verrà riaccreditato sullo stesso metodo di pagamento utilizzato per
                 l'acquisto originale una volta che Aurumè avrà ricevuto i prodotti restituiti. Tutti i prodotti restituiti sono soggetti a controllo di qualità.</p>
-            <p>E’ inoltre possibile richiedere tramite il Sito la sostituzione dei prodotti acquistati online con una taglia o una variante di colore 
+            <p>E' inoltre possibile richiedere tramite il Sito la sostituzione dei prodotti acquistati online con una taglia o una variante di colore 
                 diversa degli stessi prodotti. Per richiedere la sostituzione tramite il Sito, può compilare il modulo di reso online o accedere 
                 alla sua Cronologia ordini tramite il MyAccount, per inviare la richiesta di cambio. 
                 Sarà possibile sostituire un prodotto solo se la taglia o il colore del prodotto da sostituire è disponibile in magazzino e di pari 
@@ -194,21 +195,9 @@
                 Riceverà quindi un'e-mail separata con le nuove informazioni sull'ordine e il numero di tracciamento per il nuovo prodotto.</p>
         </section>
     </main>
-    <footer>
-        <div class="footer-column">
-            <h3>Servizio clienti</h3>
-            <p><a href="">FAQ</a></p>
-            <p><a href="Contatti.html">Contatti</a></p>
-        </div>
-        <div class="footer-column">
-            <h3><a href="#">Termini & condizioni</a></h3>
-        </div>
-        <div class="footer-column">
-            <h3>Social</h3>
-            <a href="https://www.example.com">
-                <img src="logoINSTAGRAM.webp" alt="Social Media Icon">
-            </a>
-        </div>
-    </footer>
+
+    <!-- Include the footer -->
+    <?php include 'includes/footer.html'; ?>
+
 </body>
 </html>
