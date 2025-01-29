91396bdfbdb91af426988a4e94cf235655ff6e56
Carmine Terracciano <carminet2004@gmail.com>
2025-01-29 20:19:43 +0100

    sistemati file php

--- Aurumè/faq.php
+++ Aurumè/faq.php
@@ -1,31 +1,14 @@
-<!DOCTYPE html>
-<html lang="it">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>faq</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="resources/css/header.css">
-    <link rel="stylesheet" href="resources/css/footer.css">
-    <link rel="stylesheet" href="resources/css/contatti.css">
-</head>
-<body>
-    <header>
-        <div class="left-icons">
-            <!-- Icona del menu -->
-            <i class="fas fa-bars"></i>
-        </div>
-        <div class="title">Aurumè</div>
-        <div class="right-icons">
-            <!-- Icona della lente di ingrandimento -->
-            <i class="fas fa-search"></i>
-            <!-- Icona dell'omino stilizzato -->
-            <a href="#"><i class="fas fa-user"></i></a>
-            <!-- Icona della borsa per il carrello -->
-            <a href="#"><i class="fas fa-shopping-bag"></i></a>
-        </div>
-    </header>
-    
+    <!-- Include the header -->
+    <?php 
+        $title = 'FAQ';
+        $cssFile = 'resources/css/faq.css';
+        include 'includes/header.php'; 
+    ?>
+
+    <main>
+        /*da implementare*/
+    </main>
+
 
     <!-- Include the footer -->
     <?php include 'includes/footer.html'; ?>
--- Aurumè/index.php
+++ Aurumè/index.php
@@ -1,12 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Progetto gruppo 16</title>
-    
-</head>
-<body>
-    
-</body>
-</html>
+    <!-- Include the header -->
+    <?php 
+        $title = 'Homepage';
+        $cssFile = 'resources/css/index.css';
+        include 'includes/header.php'; 
+    ?>
+
+    <main>
+        /*da implementare*/
+    </main>
+
+
+    <!-- Include the footer -->
+    <?php include 'includes/footer.html'; ?>
