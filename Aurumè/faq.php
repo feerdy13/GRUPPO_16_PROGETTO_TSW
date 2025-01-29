b932f24b45287cd4637796a0dd3cad6c3f581808
Ferdinando <ferdypaparo03@gmail.com>
2025-01-29 20:23:19 +0100

    incluso faq.php e faq.css

--- Aurumè/faq.php
+++ Aurumè/faq.php
@@ -6,7 +6,26 @@
     ?>
 
     <main>
-        /*da implementare*/
+        <h1>DOMANDE FREQUENTI</h1>
+        <div class="faq">
+            <div class="question">
+                <h2>Informazioni sull'ordine</h2>
+            </div>
+            <div class="question">
+                <h2>Spedizioni</h2>
+            </div>
+            <div class="question">
+                <h2>Reso e cambio</h2>
+            </div>
+            <div class="question">
+                <h2>Garanzie</h2>
+            </div>
+        </div>
+        <h2>Non hai trovato risposta alla tua domanda? Contattaci.</h2>
+        <div class="contatti">
+            <div class="email">E-mail</div>
+            <div class="instagram">Instagram</div>
+        </div>
     </main>
 
 
