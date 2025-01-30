c0dc350d6aecea09a96636a01a2a722d23f6e6e0
Carmine Terracciano <carminet2004@gmail.com>
2025-01-30 16:58:12 +0100

    contatti

--- Aurumè/contatti.php
+++ Aurumè/contatti.php
@@ -9,18 +9,20 @@
         <h1 class="heading">Contatti</h1>
 
         <div class="contacts-container">
-            <div class="contacts-email">
+            <div class="contacts">
                 <h2>Scrivici via email</h2>
-                <p class="email-info">Siamo qui per aiutarti! Se hai domande, richieste o necessiti di assistenza, non esitare a contattarci. Il nostro team di supporto è a tua disposizione per fornirti tutte le informazioni di cui hai bisogno.</p>
-                <p class="email"><a href="mailto:/*COMPLETA*/">Inviaci una email</a></p>
+                <p class="email-info">Siamo qui per aiutarti! Se hai domande, richieste 
+                    o necessiti di assistenza, non esitare a contattarci. Il nostro team di supporto è a tua disposizione per fornirti tutte le informazioni di cui hai bisogno.</p>
+                <p class="email"><a href="mailto:info@aurume.com">Inviaci una email</a></p>
             </div>
 
-            <div class="contacts-instagram">
+            <div class="contacts">
                 <h2>Visita il nostro profilo social</h2>
-                <p class="instagram-info"></p>
-                <p class="instagram"><a href="/*COMPLETA*/"></a></p>
+                <p class="instagram-info">Seguici sui nostri canali social per rimanere aggiornato sulle ultime novità e promozioni.</p>
+                <p class="instagram"><a href="https://www.instagram.com/yourprofile">Visita il nostro Instagram</a></p>
+            </div>
 
-            <div class="contacts-hours">
+            <div class="contacts">
                 <h2>Orari supporto online</h2>
                 <p>Lunedì: 9:00 - 19:00</p>
                 <p>Martedì: 9:00 - 19:00 </p>
