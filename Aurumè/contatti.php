9c6a39e871dda5839a02cba5b32caf831b9ab83f
Carmine Terracciano <carminet2004@gmail.com>
2025-01-30 16:47:37 +0100

    bgcolor

--- Aurumè/contatti.php
+++ Aurumè/contatti.php
@@ -6,25 +6,29 @@
     ?>
 
     <main>
-        <h1>I nostri contatti</h1>
-        <div class="container-contatti">
-            <div class="email">E-mail</div>
-            <div class="instagram">Instagram</div>
-        </div>    
+        <h1 class="heading">Contatti</h1>
 
-        <div class="link-contatti">
-            <div class="indirizzo-email">indirizzo email</div>
-            <div class="link-instagram"><a href=""></a>profilo instagram</div>
-        </div>
-        
-        <div class="orari">
-            <h2>Orari supporto online:</h2>
-            <p>Lunedì: 9:00 - 19:00</p>
-            <p>Martedì: 9:00 - 19:00 </p>
-            <p>Mercoledì: solo in ricezione</p>
-            <p>Giovedì: 9:00 - 19:00</p>
-            <p>Venerdì: 9:00 - 19:00</p>
-            <p>Sabato: 9:00 - 13:00</p>
+        <div class="contacts-container">
+            <div class="contacts-email">
+                <h2>Scrivici via email</h2>
+                <p class="email-info">Siamo qui per aiutarti! Se hai domande, richieste o necessiti di assistenza, non esitare a contattarci. Il nostro team di supporto è a tua disposizione per fornirti tutte le informazioni di cui hai bisogno.</p>
+                <p class="email"><a href="mailto:/*COMPLETA*/">Inviaci una email</a></p>
+            </div>
+
+            <div class="contacts-instagram">
+                <h2>Visita il nostro profilo social</h2>
+                <p class="instagram-info"></p>
+                <p class="instagram"><a href="/*COMPLETA*/"></a></p>
+
+            <div class="contacts-hours">
+                <h2>Orari supporto online</h2>
+                <p>Lunedì: 9:00 - 19:00</p>
+                <p>Martedì: 9:00 - 19:00 </p>
+                <p>Mercoledì: solo in ricezione</p>
+                <p>Giovedì: 9:00 - 19:00</p>
+                <p>Venerdì: 9:00 - 19:00</p>
+                <p>Sabato: 9:00 - 13:00</p>
+            </div>
         </div>
     </main>
             
