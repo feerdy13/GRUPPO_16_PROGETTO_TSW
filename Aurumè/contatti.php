09eb8159b69b6989c688713ef44166eb9c4c5f48
Carmine Terracciano <carminet2004@gmail.com>
2025-01-30 17:56:31 +0100

    completata contatti

--- Aurumè/contatti.php
+++ Aurumè/contatti.php
@@ -11,8 +11,7 @@
         <div class="contacts-container">
             <div class="contacts">
                 <h2>Scrivici via email</h2>
-                <p class="email-info">Siamo qui per aiutarti! Se hai domande, richieste 
-                    o necessiti di assistenza, non esitare a contattarci. Il nostro team di supporto è a tua disposizione per fornirti tutte le informazioni di cui hai bisogno.</p>
+                <p class="email-info">Siamo qui per aiutarti! Se hai domande, richieste o necessiti di assistenza, non esitare a contattarci. Il nostro team di supporto è a tua disposizione per fornirti tutte le informazioni di cui hai bisogno.</p>
                 <p class="email"><a href="mailto:info@aurume.com">Inviaci una email</a></p>
             </div>
 
@@ -24,14 +23,32 @@
 
             <div class="contacts">
                 <h2>Orari supporto online</h2>
-                <div class="online-support">
-                    <div class="day">Lunedì:</div>      <div class="time">9:00 - 19:00</div>
-                    <div class="day">Martedì:</div>     <div class="time">9:00 - 19:00</div>
-                    <div class="day">Mercoledì:</div>   <div class="time">solo in ricezione</div>
-                    <div class="day">Giovedì:</div>     <div class="time">9:00 - 19:00</div>
-                    <div class="day">Venerdì:</div>     <div class="time">9:00 - 19:00</div>
-                    <div class="day">Sabato:</div>      <div class="time">9:00 - 13:00</div>
-                </div>
+                <table class="hours-table">
+                    <tr>
+                        <td class="day">Lunedì:</td>
+                        <td class="time">9:00 - 19:00</td>
+                    </tr>
+                    <tr>
+                        <td class="day">Martedì:</td>
+                        <td class="time">9:00 - 19:00</td>
+                    </tr>
+                    <tr>
+                        <td class="day">Mercoledì:</td>
+                        <td class="time">solo in ricezione</td>
+                    </tr>
+                    <tr>
+                        <td class="day">Giovedì:</td>
+                        <td class="time">9:00 - 19:00</td>
+                    </tr>
+                    <tr>
+                        <td class="day">Venerdì:</td>
+                        <td class="time">9:00 - 19:00</td>
+                    </tr>
+                    <tr>
+                        <td class="day">Sabato:</td>
+                        <td class="time">9:00 - 13:00</td>
+                    </tr>
+                </table>
             </div>
         </div>
     </main>
