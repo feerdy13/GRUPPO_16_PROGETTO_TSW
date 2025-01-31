0b0be07994bd3fb8ed16121ebdee2b339b409f45
Pasquale Piccolo <p.piccolo5@studenti.unisa.it>
2025-01-31 11:23:05 +0100

    creazione pagina la nostra storia

---
+++ Aurumè/la_nostra_storia.php
@@ -0,0 +1,6 @@
+<!-- Include the header -->
+    <?php 
+        $title = 'La nostra storia';
+        $cssFile = 'resources/css/la_nostra_storia.css';
+        include 'includes/header.php'; 
+    ?>
