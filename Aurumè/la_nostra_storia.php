80b3de830a4936e00a70edcc397f290e327e59b1
Pasquale Piccolo <p.piccolo5@studenti.unisa.it>
2025-01-31 18:03:48 +0100

    fine pagina la nostra storia

--- Aurumè/la_nostra_storia.php
+++ Aurumè/la_nostra_storia.php
@@ -7,7 +7,7 @@
 
 
     <h2>Il significato del nome del brand</h2>
-    <p>
+    <p class="description">
         Aurumè è un nome che deriva dalla parola latina "aurum", che significa oro. <br>
         Abbiamo scelto questo nome perché il nostro obiettivo è quello di creare gioielli unici e preziosi, <br>
         realizzati con materiali di alta qualità e con la massima cura e dedizione.
@@ -16,18 +16,18 @@
     <div class="image-row">
         <div class="image-column">
             <img src="resources/img/artigiano.jpeg" alt="artigiano">
-            <p>Chi siamo?</p>
-            <p>Siamo un gruppo di artigiani appassionati che creano gioielli unici e personalizzati.</p>
+            <p class="question">Chi siamo?</p>
+            <p class="answer">Siamo un gruppo di artigiani appassionati che creano gioielli unici e personalizzati.</p>
         </div>
         <div class="image-column">
             <img src="resources/img/gioielli.png" alt="gioielli">
-            <p>Cosa proponiamo?</p>
-            <p>Offriamo una vasta gamma di gioielli realizzati a mano, utilizzando solo i migliori materiali.</p>
+            <p class="question">Cosa proponiamo?</p>
+            <p class="answer">Offriamo una vasta gamma di gioielli realizzati a mano, utilizzando solo i migliori materiali.</p>
         </div>
         <div class="image-column">
-            <img src="" alt="perché-sceglierci">
-            <p>Perché sceglierci?</p>
-            <p>Perché ogni nostro pezzo è unico e realizzato con la massima cura e dedizione.</p>
+            <img src="resources/img/pezzi_unici.webp" alt="perché-sceglierci">
+            <p class="question">Perché sceglierci?</p> 
+            <p class="answer">Perché ogni nostro pezzo è unico e realizzato da zero a mano con la massima cura e dedizione.</p>
         </div>
     </div>
         
