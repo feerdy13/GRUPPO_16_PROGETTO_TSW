4664595ecb61783b9089b00b6cb061f94bc7e696
Carmine Terracciano <carminet2004@gmail.com>
2025-02-02 11:22:57 +0100

    pulsante contattaci

--- Aurumè/index.php
+++ Aurumè/index.php
@@ -7,7 +7,13 @@
 
     <main>
         <video class="video-home" src="resources/img/jewellery.mp4" autoplay loop muted></video>
+
     </main>
+
+    <!-- Pulsante per inviare email -->
+    <div class="email-button-container">
+        <a href="mailto:carminet2004@gmail.com" class="email-button">Contattaci</a>
+    </div>
 
 
     <!-- Include the footer -->
