b1138d87d579735303093b82c09eb66bd41d4adc
Pasquale Piccolo <p.piccolo5@studenti.unisa.it>
2025-02-04 10:40:38 +0100

    inserimento modifiche pagina carrello

--- Aurumè/carrello.php
+++ Aurumè/carrello.php
@@ -5,8 +5,24 @@
         include 'includes/header.php'; 
     ?>
 
+    <h2 class="title">Carrello</h2>
 
-    carrello
+    <div class="container">
+        <div class="metodi-di-spezidizione">
+            <h4>Metodi di spedizione</h4>
+            <h5>Spedizione Standard</h5>
+            <p>4-5 giorni lavorativi - Gratuita per tutti gli ordini</p>
+            <h5>Spedizione Express</h5>
+            <p>1-2 giorni lavorativi - Prezzo 5 euro</p>
+        </div>
+
+        <div class="<resi-gratuiti">
+            <h4>Resi gratuiti</h4>
+            <p>Puoi restituire qualsiasi acquisto effettuato su Aurumè <br> entro 14 giorni dalla data di consegna.</p>
+        </div>
+        
+    </div>
+    
 
             
     <!-- Include the footer -->
