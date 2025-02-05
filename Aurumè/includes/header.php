27f3955581ad955e36ff912669cd54280a9f4d27
Ferdinando <ferdypaparo03@gmail.com>
2025-02-05 13:07:27 +0100

    recupero codice menu utente + riconoscimento

--- Aurumè/includes/header.php
+++ Aurumè/includes/header.php
@@ -64,10 +64,41 @@
         
         <div class="header-title"><a href="index.php">A U R U M È</a></div>
         <div class="right-icons">
-            <!-- Icona della lente di ingrandimento -->
-            <i class="fi fi-rs-search"></i>
-            <!-- Icona dell'omino stilizzato -->
-            <a href="autenticazione.php"><i class="fi fi-rs-user"></i></a>
+                <!-- Icona della lente di ingrandimento -->
+                <i class="fi fi-rs-search"></i>
+
+                <!-- Controllo se l'utente è loggato -->
+                <?php if (isset($_SESSION["user_name"])): ?>
+                        <!-- Icona dell'omino stilizzato -->
+                        <a href="javascript:void(0)" class="user-icon" onclick="toggleMenu()">
+                            <i class="fi fi-rs-user">
+                                <?php echo htmlspecialchars($_SESSION["user_name"]); ?>
+                            </i> 
+                        </a>
+                    <div class="sub-menu-wrap" id="subMenu">
+                        <div class="sub-menu">
+                            <div class="user-info">
+                                <?php echo htmlspecialchars($_SESSION["user_name"]); ?>
+                            </div>
+                            <hr>
+
+                            <a href="area_utente.php" class="sub-menu-link">
+                                <img src="resources/img/profile.png" alt="Area Utente">
+                                <p>Area Utente</p>
+                                <span>></span>
+                            </a>
+                            <a href="action/logout.php" class="sub-menu-link">
+                                <img src="resources/img/logout.png" alt="Logout">
+                                <p>Logout</p>
+                                <span>></span>
+                            </a>
+                        </div>
+                    </div>
+                    <script src="resources/js/header.js"></script>
+                <?php else: ?>
+                    <!-- Icona dell'omino stilizzato -->
+                    <a href="autenticazione.php"><i class="fi fi-rs-user"></i></a>
+                <?php endif; ?>
             <!-- Icona della borsa per il carrello -->
             <a href="carrello.php"><i class="fi fi-rs-shopping-bag"></i></a>
         </div>
