2e9908cce8244d5fe01625fe72109362e611830a
Carmine Terracciano <carminet2004@gmail.com>
2025-02-06 17:22:26 +0100

    carrello provvisorio

--- Aurumè/carrello.php
+++ Aurumè/carrello.php
@@ -6,29 +6,24 @@
             // Utente loggato: il carrello viene recuperato dal database
             $user_id = $_SESSION['user_id'];
             
-            // Dammi tutti i dettagli dei prodotti che sono attualmente nel carrello dell'utente $1
-            $query = "SELECT p.id, p.filename, p.descrizione, p.prezzo, p.categoria 
+            // Recupero tutti i dettagli dei prodotti che sono attualmente nel carrello dell'utente $1
+            $query = "SELECT p.id, p.filename, p.descrizione, p.prezzo, p.categoria, c.quantita
                         FROM carrello c 
                         JOIN prodotti p ON c.id_prodotto = p.id 
                         WHERE c.id_utente = $1";
 
             pg_prepare($conn, "estrai_carrello", $query);
             $result = pg_execute($conn, "estrai_carrello", array($user_id));
-        } else {
-            // Utente non loggato: il carrello viene recuperato dalla sessione
-            $cart = isset($_SESSION['cart']) ? $_SESSION['cart'] : array();
-            if (!empty($cart)) {
-                // $cart contiene gli id dei prodotti nel carrello dell'utente non loggato
-                // salvo gli id in una stringa, $idsString, separata da virgole
-                $ids = array_map('intval', $cart);
-                $idsString = implode(',', $ids);
 
-                $query = "SELECT id, filename, descrizione, prezzo, categoria 
-                            FROM prodotti 
-                            WHERE id IN ($idsString)";
-                $result = pg_query($conn, $query);
-            } else
-                $result = false;
+            // Calcolo il prezzo totale del carrello
+            $total = 0;
+            $cart_items = [];
+            if ($result) {
+                while ($row = pg_fetch_assoc($result)) {
+                    $cart_items[] = $row;
+                    $total += $total += round($row['prezzo'] * $row['quantita'], 2); // Arrotonda a due decimali
+                }
+            }
         }
     ?>
     
@@ -42,44 +37,98 @@
     ?>
     
     <main>
+        
         <h1 class="heading">Carrello</h2>
+
         <?php if (isset($_SESSION['user_id'])): ?>
-            <?php if (!$result || pg_num_rows($result) == 0): ?>
+            <!-- Utente loggato: ha accesso al carrello -->
+            
+            <?php if (pg_num_rows($result) == 0): ?>
+                <!-- Utente loggato: carrello vuoto -->
                 <div class='text-center'>
                     <h2>Il tuo carrello è vuoto</h2>
                     <h3>Aggiungi dei prodotti al carrello dalla pagina del catalogo</h3>
                 </div>
-            <?php else: ?>
-                <div class="cart-container">
-                    <?php while ($row = pg_fetch_assoc($result)): ?>
-                        <div class="cart-item">
-                            <img src="resources/img/catalogue/<?php echo htmlspecialchars($row['filename']); ?>" alt="Immagine prodotto">
-                            <div class="cart-info">
-                                <p class="descrizione"><?php echo htmlspecialchars($row['descrizione']); ?></p>
-                                <p class="prezzo"><?php echo htmlspecialchars($row['prezzo']); ?> €</p>
-                                <p class="categoria"><?php echo htmlspecialchars($row['categoria']); ?></p>
-                            </div>
-                            <a href="action/rimuovi_dal_carrello.php?product_id=<?php echo $row['id']; ?>" class="remove-from-cart">Rimuovi</a>
-                        </div>
-                    <?php endwhile; ?>
-                </div>
-            <?php endif; ?> 
 
+            <?php else: ?>
+                <!-- Utente loggato: carrello non vuoto -->
+
+                <!-- visualizzo i prodotti nel carrello -->
+                <div class="cart-page-container">
+
+                    <div class="cart-items-container">
+                        <?php foreach ($cart_items as $row): ?>
+                            <!-- Recupero il contenuto del carrello -->
+
+                            <div class="cart-item">
+                                <img src="resources/img/catalogue/<?php echo htmlspecialchars($row['filename']); ?>" alt="Immagine prodotto">
+                                <div class="cart-info">
+                                    <div class="descrizione"><?php echo $row['descrizione']; ?></div>
+                                    <div class="quantity-control">
+                                        <button class="quantity-btn" onclick="updateQuantity(<?php echo $row['id']; ?>, -1)">-</button>
+                                        <span class="quantita"><?php echo $row['quantita']; ?></span>
+                                        <button class="quantity-btn" onclick="updateQuantity(<?php echo $row['id']; ?>, 1)">+</button>
+                                    </div>
+                                    <p class="prezzo"><?php echo number_format($row['prezzo'], 2); ?> €</p>
+                                    <a href="action/rimuovi_dal_carrello.php?product_id=<?php echo $row['id']; ?>" class="remove-from-cart">Rimuovi</a>
+                                </div>
+                            </div>
+
+                        <?php endforeach; ?>
+                    </div>
+
+                    <!-- Visualizzo il totale del carrello -->
+                    <div class="order-summary">
+                        <h1>Riepilogo ordine</h1>
+                        <div class="summary-details">
+                            <div class="summary-row total">
+                                <strong>Totale</strong>
+                                <strong><?php echo number_format($total, 2); ?> €</strong>
+                            </div>
+                            <div class="summary-row">
+                                <span>Incluse tasse e spese di spedizione</span>
+                            </div>
+
+                            <button class="complete-purchase">COMPLETA L'ACQUISTO</button>
+
+                            <div class="summary-row">
+                                <ul>
+                                    <li>Spedizione standard de 1 a 2 giorni lavorativi, gratuita su tutti gli ordini.</li>
+                                    <li>Servizio di ritiro in una qualsiasi boutique Bvlgari in Italia. Il tuo ordine online sarà disponibile per il ritiro entro 3 giorni dalla data di acquisto.</li>
+                                    <li>Puoi restituire una creazione Bvlgari acquistata online e richiedere un cambio o un rimborso entro 14 giorni dalla data di consegna.</li>
+                                </ul>
+                            </div>
+                           
+                        </div>
+                    </div>
+
+                </div>
+
+            <?php endif; ?> 
+            
+            <!-- Utente loggato: visualizza il bottone per tornare al catalogo -->
             <div class="button">
                 <a href='catalogo.php'>Torna al catalogo</a>
             </div>
 
         <?php else: ?> 
+            <!-- Utente non loggato: non ha accesso al carrello -->
             <div class='text-center'>
                 <h2>Ops!</h2>
                 <h3>Devi aver effettuato l'accesso per accedere al carrello</h3>
             </div> 
+
+            <!-- Utente non loggato: visualizza il bottone per effettuare l'accesso -->
             <div class="button">
                 <a href='autenticazione.php'>Accedi</a>
             </div>
+
         <?php endif; ?>
+
     </main>
     
+
+
     <!-- Chiudo la connessione -->
     <?php pg_close($conn); ?>
  
